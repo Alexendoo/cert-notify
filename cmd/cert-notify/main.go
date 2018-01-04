250308cda9479476075cafe04fdcf87ce754ea43
Alex Macleod <alex@macleod.io>
2018-01-04 22:57:17 +0000

    initial ctlog scanning implementation

--- cmd/cert-notify/main.go
+++ cmd/cert-notify/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
-	"net/http"
+	"time"
 
-	"github.com/Alexendoo/cert-notify/frontend"
-	"github.com/Alexendoo/cert-notify/store"
-	"github.com/gauntface/web-push-go/webpush"
-	"github.com/google/certificate-transparency-go/client"
-	"github.com/google/certificate-transparency-go/jsonclient"
+	"github.com/Alexendoo/cert-notify/ctlog"
 )
 
 func debase64(encoded string) []byte {
@@ -23,21 +20,32 @@ func debase64(encoded string) []byte {
 }
 
 func main() {
-	ctClient, err := client.New("https://ct.googleapis.com/logs/argon2018", http.DefaultClient, jsonclient.Options{
-		PublicKeyDER: debase64("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE0gBVBa3VR7QZu82V+ynXWD14JM3ORp37MtRxTmACJV5ZPtfUA7htQ2hofuigZQs+bnFZkje+qejxoyvk2Q1VaA=="),
-	})
+	// ctClient, err := client.New("https://ct.googleapis.com/logs/argon2018", http.DefaultClient, jsonclient.Options{
+	// 	PublicKeyDER: debase64("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE0gBVBa3VR7QZu82V+ynXWD14JM3ORp37MtRxTmACJV5ZPtfUA7htQ2hofuigZQs+bnFZkje+qejxoyvk2Q1VaA=="),
+	// })
 
-	fmt.Printf("ctClient: %v\n", ctClient)
-	fmt.Printf("err: %v\n", err)
+	// fmt.Printf("ctClient: %v\n", ctClient)
+	// fmt.Printf("err: %v\n", err)
 
-	s, err := store.New("z.db")
-	fmt.Println(err)
-	sub := &webpush.Subscription{
-		Auth:     []byte{0x30, 0x32, 0x53, 0x9F, 0xe1, 0xff, 0x00},
-		Endpoint: "https://example.org/push/awioefioawioef",
-		Key:      []byte("Secret key"),
+	// s, err := store.New("z.db")
+	// fmt.Println(err)
+	// sub := &webpush.Subscription{
+	// 	Auth:     []byte{0x30, 0x32, 0x53, 0x9F, 0xe1, 0xff, 0x00},
+	// 	Endpoint: "https://example.org/push/awioefioawioef",
+	// 	Key:      []byte("Secret key"),
+	// }
+	// fmt.Println(s.AddDomain("macleod.io", sub))
+
+	// frontend.Serve()
+
+	log := &ctlog.Log{
+		Key: debase64("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE0gBVBa3VR7QZu82V+ynXWD14JM3ORp37MtRxTmACJV5ZPtfUA7htQ2hofuigZQs+bnFZkje+qejxoyvk2Q1VaA=="),
+		URL: "https://ct.googleapis.com/logs/argon2018",
 	}
-	fmt.Println(s.AddDomain("macleod.io", sub))
 
-	frontend.Serve()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err := log.Scan(ctx)
+	fmt.Println(err)
 }
--- ctlog/log.go
+++ ctlog/log.go
@@ -1,6 +1,103 @@
 package ctlog
 
-// Log is a CT log
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/certificate-transparency-go"
+	"github.com/google/certificate-transparency-go/client"
+	"github.com/google/certificate-transparency-go/jsonclient"
+)
+
+// Log is an individual CT log
+//
+// - https://www.certificate-transparency.org/known-logs
 type Log struct {
-	URL string `json:"url"`
+	// Start index of the next log retrieval, either the end index of the last
+	// retrieval or the tree size of a new log
+	Index int64
+	// DER format of the log's public key used for signature verfication
+	Key []byte
+	URL string
+
+	c *client.LogClient
+}
+
+func (log *Log) setupClient() (err error) {
+	// TODO: not http.DefaultClient
+	log.c, err = client.New(log.URL, http.DefaultClient, jsonclient.Options{
+		PublicKeyDER: log.Key,
+	})
+
+	return
+}
+
+func (log *Log) fetchIndex(ctx context.Context) error {
+	sth, err := log.c.GetSTH(ctx)
+	if err != nil {
+		return err
+	}
+
+	log.Index = int64(sth.TreeSize)
+
+	return nil
+}
+
+func (log *Log) Scan(ctx context.Context) error {
+	if log.c == nil {
+		err := log.setupClient()
+		if err != nil {
+			return err
+		}
+	}
+
+	if log.Index == 0 {
+		err := log.fetchIndex(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	delay := 5 * time.Second
+	done := ctx.Done()
+
+	for {
+		select {
+		case <-done:
+			return nil
+		case <-time.After(delay):
+			log.scan(ctx)
+		}
+	}
+}
+
+func (log *Log) scan(ctx context.Context) {
+	sth, err := log.c.GetSTH(ctx)
+	if err != nil {
+		return
+	}
+
+	raw, err := log.c.GetRawEntries(ctx, log.Index, int64(sth.TreeSize))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i, leaf := range raw.Entries {
+		index := log.Index + int64(i)
+		entry, err := ct.LogEntryFromLeaf(index, &leaf)
+		if err != nil {
+			fmt.Println(log.URL, err)
+			continue
+		}
+
+		// TODO: decide on concurrency model for sending notifications to push
+		// services
+
+		if entry.X509Cert != nil {
+			fmt.Println(entry.X509Cert.DNSNames)
+		}
+	}
 }
